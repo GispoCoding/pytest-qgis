248184d627d78cc38045b0ffedaad5a2fa688f1c
Lauri Kajan <lauri.kajan@gispo.fi>
2021-10-06 13:54:25 +0300

    Use empty config path for QGIS

--- src/pytest_qgis/pytest_qgis.py
+++ src/pytest_qgis/pytest_qgis.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
+from pathlib import Path
+from typing import TYPE_CHECKING
+from unittest.mock import patch
+
 import pytest
 from qgis.core import QgsApplication
-from qgis.gui import QgisInterface as QgisInterfaceOrig
 from qgis.gui import QgsMapCanvas
 from qgis.PyQt import QtCore, QtWidgets
 from qgis.PyQt.QtWidgets import QWidget
@@ -10,11 +13,21 @@ from qgis.PyQt.QtWidgets import QWidget
 from pytest_qgis.mock_qgis_classes import MainWindow, MockMessageBar
 from pytest_qgis.qgis_interface import QgisInterface
 
+if TYPE_CHECKING:
+    from _pytest.tmpdir import TempPathFactory
+    from qgis.gui import QgisInterface as QgisInterfaceOrig
+
+
+@pytest.fixture(scope="session")
+def tmp_qgis_config_path(tmp_path_factory: "TempPathFactory") -> Path:
+    config_path = tmp_path_factory.mktemp("qgis-test")
+    with patch.dict("os.environ", {"QGIS_CUSTOM_CONFIG_PATH": str(config_path)}):
+        yield config_path
+
 
 @pytest.fixture(autouse=True, scope="session")
-def qgis_app() -> QgsApplication:
+def qgis_app(tmp_qgis_config_path: "Path") -> QgsApplication:
     """Initializes qgis session for all tests"""
-
     app = QgsApplication([], GUIenabled=False)
     app.initQgis()
 
@@ -36,7 +49,7 @@ def qgis_canvas(qgis_parent: QWidget) -> QgsMapCanvas:
 
 
 @pytest.fixture(scope="session")
-def qgis_iface(qgis_canvas: QgsMapCanvas) -> QgisInterfaceOrig:
+def qgis_iface(qgis_canvas: QgsMapCanvas) -> "QgisInterfaceOrig":
     # QgisInterface is a stub implementation of the QGIS plugin interface
     iface = QgisInterface(qgis_canvas, MockMessageBar(), MainWindow())
     return iface
