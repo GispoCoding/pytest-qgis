c8a6c6b1c1ab3102dbb072158e1c323dabbae475
Joonalai <33314057+Joonalai@users.noreply.github.com>
2021-10-07 10:45:20 +0300

    Prepare for 1.0.0 release (#3)
    * Add flake8-pytest-style and update flake8-qgis
    
    * Add type hints to mocked classes
    
    * Add copyright information
    
    * Add downloads badge
    
    * Set development status to Production/Stable

--- src/pytest_qgis/mock_qgis_classes.py
+++ src/pytest_qgis/mock_qgis_classes.py
@@ -1,10 +1,32 @@
-from typing import Dict, List
+#  Gispo Ltd., hereby disclaims all copyright interest in the program pytest-qgis
+#  Copyright (C) 2021 Gispo Ltd (https://www.gispo.fi/).
+#
+#
+#  This file is part of pytest-qgis.
+#
+#  pytest-qgis is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  pytest-qgis is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with pytest-qgis.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+from typing import Any, Dict, List
 
 from qgis.core import Qgis
 from qgis.PyQt.QtCore import QObject
 
 
 class MockMessageBar(QObject):
+    """Mocked message bar to hold the messages."""
+
     def __init__(self) -> None:
         super().__init__()
         self.messages: Dict[int, List[str]] = {
@@ -15,14 +37,18 @@ class MockMessageBar(QObject):
         }
 
     def get_messages(self, level: int) -> List[str]:
-        """Used to test which messages have been logged"""
+        """Used to test which messages have been logged."""
         return self.messages[level]
 
-    def pushMessage(self, title, text, level, duration):  # noqa N802
+    def pushMessage(  # noqa N802
+        self, title: str, text: str, level: int, duration: int
+    ) -> None:
+        """A mocked method for pushing a message to the bar."""
         msg = f"{title}:{text}"
         self.messages[level].append(msg)
 
 
 class MainWindow(QObject):
-    def blockSignals(self, *args):  # noqa N802
+    def blockSignals(self, *args: Any) -> None:  # noqa N802
+        """Mocked blockSignals"""
         pass
--- src/pytest_qgis/pytest_qgis.py
+++ src/pytest_qgis/pytest_qgis.py
@@ -1,5 +1,25 @@
 # -*- coding: utf-8 -*-
 
+#  Gispo Ltd., hereby disclaims all copyright interest in the program pytest-qgis
+#  Copyright (C) 2021 Gispo Ltd (https://www.gispo.fi/).
+#
+#
+#  This file is part of pytest-qgis.
+#
+#  pytest-qgis is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  pytest-qgis is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with pytest-qgis.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 import os.path
 import sys
 from pathlib import Path
@@ -58,7 +78,7 @@ def qgis_iface(qgis_canvas: QgsMapCanvas) -> QgisInterfaceOrig:
     return iface
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture()
 def new_project(qgis_iface: QgisInterface) -> None:  # noqa QGS105
     """
     Initializes new QGIS project by removing layers and relations etc.
