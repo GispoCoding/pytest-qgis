93c6a6572d5013d13d846e967cfa6a13bbf33cea
Joonalai <joona.laine@cofactor.fi>
2023-12-18 22:57:16 +0200

    Investigate breaking QGIS version with crs copying

--- tests/test_crs.py
+++ tests/test_crs.py
@@ -1,30 +1,32 @@
 import unittest
 from pathlib import Path
 
-from qgis.core import QgsApplication, QgsRasterLayer, QgsVectorLayer
+from qgis.core import (
+    QgsApplication,
+    QgsCoordinateReferenceSystem,
+    QgsRasterLayer,
+    QgsVectorLayer,
+)
 
 app = QgsApplication([], GUIenabled=False)
 
 
 class TestCrs(unittest.TestCase):
-    def setUp(self):
-        # This works with both unittest and pytest
-        # app = QgsApplication([], GUIenabled=False)
-
-        # This makes both unittest and pytest fail
-        # self._app = QgsApplication([], GUIenabled=False)
-        pass
+    def test_crs_copying(self):
+        crs1 = QgsCoordinateReferenceSystem("EPSG:4326")
+        assert crs1.isValid()
+        crs2 = QgsCoordinateReferenceSystem.fromWkt(crs1.toWkt())
+        assert crs1.toWkt() == crs2.toWkt()
+        assert crs2.isValid()  # <-- raises error
 
     def test_crs_raster(self):
         raster_path = Path(Path(__file__).parent, "data", "small_raster.tif")
         layer = QgsRasterLayer(raster_path.as_posix())
         assert layer.crs().toWkt()
-        print(layer.crs())  # noqa: T201
         assert layer.crs().isValid()
 
     def test_crs_vector(self):
         gpkg = Path(Path(__file__).parent, "data", "db.gpkg")
         layer = QgsVectorLayer(f"{gpkg}|layername=polygon_3067", "layer", "ogr")
         assert layer.crs().toWkt()
-        print(layer.crs())  # noqa: T201
         assert layer.crs().isValid()
