06fbc617ee15f812e3bad48bb08bfe678aae7d58
Joonalai <joona.laine@cofactor.fi>
2023-12-18 22:34:23 +0200

    Investigate breaking QGIS version with unittest version

--- tests/test_crs.py
+++ tests/test_crs.py
@@ -1,12 +1,30 @@
+import unittest
 from pathlib import Path
 
-from qgis.core import QgsApplication, QgsRasterLayer
+from qgis.core import QgsApplication, QgsRasterLayer, QgsVectorLayer
+
+app = QgsApplication([], GUIenabled=False)
 
 
-def test_crs():
-    app = QgsApplication([], GUIenabled=False)  # noqa: F841
-    raster_path = Path(Path(__file__).parent, "data", "small_raster.tif")
-    layer = QgsRasterLayer(raster_path.as_posix())
-    assert layer.crs().toWkt()
-    print(layer.crs())  # noqa: T201
-    assert layer.crs().isValid()
+class TestCrs(unittest.TestCase):
+    def setUp(self):
+        # This works with both unittest and pytest
+        # app = QgsApplication([], GUIenabled=False)
+
+        # This makes both unittest and pytest fail
+        # self._app = QgsApplication([], GUIenabled=False)
+        pass
+
+    def test_crs_raster(self):
+        raster_path = Path(Path(__file__).parent, "data", "small_raster.tif")
+        layer = QgsRasterLayer(raster_path.as_posix())
+        assert layer.crs().toWkt()
+        print(layer.crs())  # noqa: T201
+        assert layer.crs().isValid()
+
+    def test_crs_vector(self):
+        gpkg = Path(Path(__file__).parent, "data", "db.gpkg")
+        layer = QgsVectorLayer(f"{gpkg}|layername=polygon_3067", "layer", "ogr")
+        assert layer.crs().toWkt()
+        print(layer.crs())  # noqa: T201
+        assert layer.crs().isValid()
